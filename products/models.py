b834014328df4f3e4fe7bb42546ac0666a5f3cfa
lmox88 <lmox88.0@gmail.com>
2026-06-12 10:11:58 +0300

    Update models.py

--- products/models.py
+++ products/models.py
@@ -19,12 +19,8 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField(blank=True, null=True)
-
+    # تأكد من هذا الجزء في models.py
+    image = models.ImageField(upload_to='products/')
     def __str__(self):
         return self.name
-class ProductImage(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
-    image = models.ImageField(upload_to='products/')
 
-    def __str__(self):
-        return f"Image for {self.product.name}"
