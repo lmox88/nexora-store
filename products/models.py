ea732ec5b208328593daa5fd05990eb064cfcf2b
lmox88 <lmox88.0@gmail.com>
2026-06-12 09:19:02 +0300

    Update models.py

--- products/models.py
+++ products/models.py
@@ -19,7 +19,6 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField(blank=True, null=True)
-    image = models.CharField(max_length=255, blank=True)
 
     def __str__(self):
         return self.name
