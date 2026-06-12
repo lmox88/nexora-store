daed367d7cc10cca5411e0a05dcdb19adb96c4b4
lmox88 <lmox88.0@gmail.com>
2026-06-12 07:23:00 +0300

    Update settings.py

--- softpalm_store/settings.py
+++ softpalm_store/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
