5ecf24c048881be82cc7bc5ff14894afc181a262
laila albusayr <431201647@qu.edu.sa>
2026-06-13 05:57:49 +0300

    Configure database, email, and cloud storage settings

--- products/views.py
+++ products/views.py
@@ -1,7 +1,9 @@
 
 from django.shortcuts import render, get_object_or_404
 from .models import Product
-
+from django.contrib.auth.models import User
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
 def product_detail(request, pk):
     product = get_object_or_404(Product, id=pk)
     return render(request, 'products/product_detail.html', {
